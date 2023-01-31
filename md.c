b0c30b5e2e65bf368d92924e31e403491af09552
rng <ray@perfectcast.com>
2023-01-31 20:26:00 +0000

    fixed license notice formatting

--- md.c
+++ md.c
@@ -2,8 +2,7 @@
 Copyright 2023 Ray Garner
 
 This program is free software: you can redistribute it and/or modify it under 
-the 
-terms of the GNU General Public License as published by the Free Software 
+the terms of the GNU General Public License as published by the Free Software 
 Foundation, either version 3 of the License, or (at your option) any later 
 version.
 
